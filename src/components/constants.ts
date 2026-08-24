0dc4024198888a171fd3d665e55a1da6a4d95182
Dyad <git@dyad.sh>
2026-08-24 15:28:01 +0300

    Fixed constants file to properly export WhatsApp_LOGO - wrote 1 file(s)

--- src/components/constants.ts
+++ src/components/constants.ts
@@ -4,3 +4,6 @@ export const WHATSAPP_NUMBER = "+966XXXXXXXX"; // To be replaced with actual num
 // Social media links
 export const INSTAGRAM_URL = "https://www.instagram.com/rama_alarab_glass";
 export const TIKTOK_URL = "https://www.tiktok.com/@rama.alarab.glass";
+
+// WhatsApp logo (emoji)
+export const WhatsApp_LOGO = "📱";
